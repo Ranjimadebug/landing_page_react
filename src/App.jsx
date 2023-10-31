03d3aefb7c36b8619f0b211c9511cb5e4ee58a70
Ranjima_Ghosh <65239271+Ranjimadebug@users.noreply.github.com>
2023-10-31 12:16:37 +0530

    add: first changes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navigation from "./components/Navigation";
+import './App.css';
+import Hero from "./components/Hero";
+
+const App = () => {
+
+return (
+ <div className="container">
+  <Navigation />
+  <Hero />
+ </div>
+)
+
+}
+
+export default App;
